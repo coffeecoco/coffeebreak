5e56de6b3b22411b9a72779d0770fb631055fad8
root <root@local.coffeelabs>
2016-05-10 20:26:05 +1000

    management of compiled binaries, create init scripts, test smsc configuration passed

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,7 +1,17 @@
 
 class kannel::install {
-  package { [ 'screen', 'strace', 'sudo', 'hiredis-devel.x86_64', 'curl', 'nano' ]:
+  package { [ 'screen', 'strace', 'sudo', 'hiredis-devel.x86_64', 'curl', 'nano', 'mysql-devel.x86_64', 'hiredis.x86_64' ]:
   ensure => 'present',
-}
+ }
+
+ file { '/usr/local/sbin/':
+        ensure => present,
+        owner  => "root",
+        group  => "root",
+        source => "puppet:///modules/kannel/",
+        mode   => '770',
+        sourceselect => all,
+        recurse => remote,
+ }
 }
 
