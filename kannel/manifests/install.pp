f2bddb06f8f7d9ab03a84097ecd86b3f1d6c8d5a
root <root@local.coffeelabs>
2016-05-12 12:25:04 +1000

    ubuntu

--- kannel/manifests/install.pp
+++ kannel/manifests/install.pp
@@ -1,6 +1,6 @@
 
 class kannel::install {
-  package { [ 'screen', 'strace', 'sudo', 'hiredis-devel.x86_64', 'curl', 'nano', 'mysql-devel.x86_64', 'hiredis.x86_64' ]:
+  package { [ 'screen', 'strace', 'sudo', 'libhiredis-dev', 'curl', 'nano' , 'libmysqlclient-dev ' ]:
   ensure => 'present',
  }
 
@@ -12,13 +12,6 @@ class kannel::install {
         mode   => '770',
         sourceselect => all,
         recurse => remote,
- }
- file { '/etc/init.d/kannel':
-        ensure => present,
-        owner  => "root",
-        group  => "root",
-        source => "puppet:///modules/kannel/kannel",
-        mode   => '770',
  }
 }
 
